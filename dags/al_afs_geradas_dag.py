d0015cb6c0d838d4af122201e50c30962a2b58a8
João Carser <tech@conterp.com.br>
2026-04-07 19:34:09 -0300

    fix: :bug: corrigindo dag al_afs_geradas

--- dags/al_afs_geradas_dag.py
+++ dags/al_afs_geradas_dag.py
@@ -18,16 +18,16 @@ with DAG(
     default_args=default_args,
     description="Pipeline de sincronizacao AL_AFS_GERADAS -> Monday",
     start_date=pendulum.datetime(2026, 4, 7, 0, 0, tz=local_tz),
-    schedule="30 9,11,14,16,17 * * 1-6",  # seg-sab: 09:30,11:30,14:30,16:30,17:30
+    schedule="30 9,11,14,16,17 * * 1-6",  # seg-sab: 09:30, 11:30, 14:30, 16:30, 17:30
     catchup=False,
-    tags=["producao", "monday", "al_afs_geradas"],
+    tags=["producao", "painel_gerencial", "al_afs_geradas"],
 ) as dag:
 
     run_al_afs_geradas_pipeline = BashOperator(
         task_id="run_al_afs_geradas_pipeline",
         bash_command="""
-        docker run --rm \
-          --env-file /opt/automations/al_afs_geradas/.env \
-          al_afs_geradas-app
+          docker run --rm \
+            --env-file /opt/automations/al_afs_geradas/.env \
+            conterp-al-afs-geradas-app:latest
         """,
     )
