6725eaddc5fd4520a2b140ac0db20af9575c93a6
João Carser <tech@conterp.com.br>
2026-06-19 16:29:55 -0300

    fix: :bug: pequena mudança data inicio

--- dags/veloe_sync_dag.py
+++ dags/veloe_sync_dag.py
@@ -19,7 +19,7 @@ with DAG(
     dag_id="veloe_sync",
     default_args=default_args,
     description="Pipeline de sincronizacao Veloe -> Monday",
-    start_date=pendulum.datetime(2026, 6, 19, 0, 0, tz=local_tz),
+    start_date=pendulum.datetime(2026, 6, 10, 0, 0, tz=local_tz),
     schedule="0 5,9,16 * * *",
     catchup=False,
     tags=["producao", "veloe", "gestao_frotas"],
